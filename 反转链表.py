a5a7b03df66611a40e543b48572bdbfd3f62382b
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2018-12-11 11:53:29 +0800

    Update 反转链表.py

--- 反转链表.py
+++ 反转链表.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 # 输入一个链表，反转链表后，输出新链表的表头。
-
+# 思路:新建一个头结点，遍历原链表，把每个节点用头结点插入到新建链表中。最后，新建的链表就是反转后的链表。
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
@@ -13,9 +13,11 @@ class Solution:
         if pHead==None or pHead.next==None:
             return pHead
         last = None
+        # pHead是要插入到新链表的节点
+        # tmp是临时保存的pHead的next
         while pHead:
-            tmp = pHead.next
-            pHead.next = last
-            last = pHead
-            pHead = tmp
+            tmp = pHead.next    # tmp保存下一次要插入的节点
+            pHead.next = last   # 把pHead插入到last中
+            last = pHead        # 纠正头结点last的指向
+            pHead = tmp         # pHead指向下一次要插入的节点
         return last
